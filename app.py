312c65886ff0baacedd19b20159f3988ef296083
Dasuni-Uthpala <68235080+Dasuni-Uthpala@users.noreply.github.com>
2020-09-18 15:23:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import pickle
+
+
+app = Flask(__name__)
+api = Api(app)
+
+model = pickle.load(open("soil_model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return "Welcome to Plantina API!"
+
+@app.route("/predict", methods=["POST","GET"])
+def post():
+        posted_data=request.get_json(force=True)
+        ph_value = posted_data['pH']
+        temperature_value = posted_data['Temperature']
+
+        data=([[ph_value,temperature_value]])
+        prediction = model.predict(data)[0]
+
+        return jsonify({
+                'prediction':
+                prediction
+            })
+
+
+#api.add_resource(MakePrediction, '/predict', methods=["POST","GET"])
+
+if __name__ == '__main__':
+        app.run(port=1235)
