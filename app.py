39a9580eabd6cdb3badacf80121087c41805d7a4
Dasuni-Uthpala <68235080+Dasuni-Uthpala@users.noreply.github.com>
2020-09-20 20:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,18 +19,12 @@ model = pickle.load(open("soil_model.pkl","rb"))
 def home():
     return "Welcome to Plantina mobile API!"
 class SoilPrediction(Resource):
-    def get(self):
+    def post(self):
         info = request.get_json()
-        #pH = float(info[0])
-        #Temperature = int(info[1])
         ph_value = info['pH']
         temperature_value = info['Temperature']
     
         data = ([[ph_value, temperature_value]])
-        #col = ['pH', 'Temperature']
-        #data = np.array(data)
-        #data = pd.DataFrame(data,columns = col)
-        #data = data_scaling(data)
         prediction = model.predict(data)[0]
        
         return {'prediction':prediction}
@@ -39,23 +33,6 @@ class SoilPrediction(Resource):
 
 
 API_NAME.add_resource(SoilPrediction, '/predict', methods=['POST', 'GET'])
-
-# def load_model():
-#     """This function load the pickled model as API for flask
-#          :returns: loaded_model
-#          :rtype: float64
-#          method's invoked:  None"""
-#     loaded_model = pickle.load(open('soil_model.pkl', 'rb'))
-#     return loaded_model
-
-
-# def data_scaling(df):
-
-#     cont_data = df.values
-#     m = open('scaled_model.pkl', 'rb')
-#     model = pickle.load(m)
-#     cont_data = model.transform(cont_data)
-#     return cont_data
 
 
 if __name__ == '__main__':
